713be59e404d23d0dc45db447748a05cc737d244
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-05-01 21:11:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoding] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newtours = tours.filter((tour) => tour.id !== id);
+    setTours(newtours);
+  }
+
+  const fetchTours = async () => {
+    setLoding(true);
+
+    try{
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoding(false);
+      setTours(tours);
+    }catch (error) {
+      setLoding(false);
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if(tours.length === 0) {
+    return <main>
+      <div className='title'>
+        <h2>no tours left</h2>
+        <button className='btn' onClick={fetchTours}>refresh</button>
+      </div>
+    </main>
+  }
+  return <main>
+    <Tours tours={tours} removeTour={removeTour}/>
+  </main>
 }
 
-export default App;
+export default App
